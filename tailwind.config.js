f4ea5f9ae633fda5f95ef721ae685e4f450fe59b
JAVIS\markc <lawlietstudio@gmail.com>
2026-08-23 19:02:02 -0400

    chore(tailwind): restrict content paths to source files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
